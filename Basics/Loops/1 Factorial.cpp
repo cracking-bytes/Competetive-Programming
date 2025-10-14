4e2ecb9f24a4517e902ed3e5064e95136aee8553
cracking-bytes <bhavikanagdeo83@gmail.com>
2025-10-14 19:13:45 +0530

    Factorial

---
+++ Basics/Loops/1 Factorial.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin >> t;
+    for(int i=1; i<=t; i++){
+        int n;
+        long long r = 1;
+        cin >> n;
+        for(int i=1; i<=n; i++) r = r*i;
+        cout << r << endl;
+    }
+
+}
