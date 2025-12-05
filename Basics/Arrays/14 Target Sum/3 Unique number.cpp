32b8b8bade74e555a5ffb8fb993da8f790ddad1a
Bhavika Nagdeo <alien@Brainiac.home>
2025-12-05 05:37:51 +0530

    Unique number

---
+++ Basics/Arrays/14 Target Sum/3 Unique number.cpp
@@ -0,0 +1,25 @@
+// Find the unique number in a given Array where all the
+// elements are being repeated twice with one value
+// being unique.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+
+	int arr[n];
+	for(int &i:arr) cin >> i;
+
+	for(int i=0; i<=n; i++){
+		for(int j=i+1; j<=n; j++){
+			if(arr[i] == arr[j]){
+				arr[i] = -1;
+				arr[j] = -1;
+			}
+		}
+	}
+
+	for(int &i:arr) (i==-1) ? cout << "" : cout << i;
+}
